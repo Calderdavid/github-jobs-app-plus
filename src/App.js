dfef39724fdcd431562019faa8152ffc9c57ec71
David Calderon <davidjcr200040@gmail.com>
2022-05-23 20:16:28 -0400

    search view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import head from './utilities/backgroundImg.png'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1><strong>Github</strong> Jobs</h1>
+                </div>
+            </div>
+            <form className="row justify-content-center">
+                <img src={head} alt="head" className="col-md-12 position-relative" />
+                <input type="text" className="ip col-6 position-relative" placeholder="Search for a job" />
+            </form>
+
+        </div>
+            
+        
+    )
 }
 
-export default App;
+export default App
