43b6ca610de51be701f960056301a215c50f7cf6
David Calderon <davidjcr200040@gmail.com>
2022-05-23 23:59:19 -0400

    header, aside views

--- src/App.js
+++ src/App.js
@@ -15,9 +15,41 @@ const App = () => {
             </div>
             <form className="row justify-content-center">
                 <img src={head} alt="head" className="col-md-12 position-relative" />
-                <input type="text" className="ip col-6 position-relative" placeholder="Search for a job" />
+                <input type="text" className="col-6 position-absolute mt-5" 
+                placeholder="Tittle, companies, expertise or benefits" 
+                autoComplete='off'/>
+                <button className="btn-search btn btn-primary col-1 position-absolute mt-5">Search</button>
             </form>
 
+            <section className="row">
+                <aside className="col-4 mt-5">
+                    <input className="form-check-input" type="checkbox" id="flexCheckDefault"/>
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Full time
+                    </label>
+
+                    <p className="text-secondary mt-4">LOCATION</p>
+                    <input type="text" className="col-3 position-absolute mt-1" 
+                    placeholder="City, state, zip code or country" 
+                    autoComplete='off'
+                    />
+
+                    <div className="row mt-4">
+                        <div className="col-1 mt-5">
+                            <input className="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                            <input className="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                            <input className="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                        </div>
+                        <div className="col-3 mt-5">
+                            <label className="form-check-label " htmlFor="flexRadioDefault1">London</label>
+                            <label className="form-check-label " htmlFor="flexRadioDefault1">Amsterdam</label>
+                            <label className="form-check-label " htmlFor="flexRadioDefault1">New York</label>
+                            <label className="form-check-label " htmlFor="flexRadioDefault1">Berlín</label>
+                        </div>
+                    </div>
+                </aside>
+            </section>
         </div>
             
         
